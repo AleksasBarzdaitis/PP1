8420ce816acf0d95df10c7e8ae8f3eae905346fa
Aleksas <a.barzdaitis@gmail.com>
2022-06-12 13:38:12 +0300

    Improved scraping speed

--- src/main.py
+++ src/main.py
@@ -6,10 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-import re
-import html
 
-
+#Loading a scraper
 url = 'https://www.aruodas.lt/namai/vilniuje/antakalnyje/'
 op = webdriver.FirefoxOptions()
 ffdriver = webdriver.Firefox(service=FFService('src/drivers/geckodriver_v0.31.0.exe'), options=op)
@@ -22,11 +20,15 @@ wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, target_button)))
 cookies_button = ffdriver.find_element(By.CSS_SELECTOR, target_button)
 cookies_button.click()
 
+#Defining scrape method
 def get_data():
     rows = ffdriver.find_elements(By.CSS_SELECTOR, '*.list-row')
     data = []
 
-    time.sleep(2)
+    wait.until(EC.text_to_be_present_in_element(
+        (By.CSS_SELECTOR, '#actionBarAdverts > div > h1'),
+        'Namai Vilniuje, Antakalnyje'))
+
     for row in rows:
         try:
             address = row.find_element(By.CSS_SELECTOR, 'td.list-adress > h3 > a')
@@ -38,14 +40,14 @@ def get_data():
         raw_text = "%r"%address.text
         split_raw = raw_text.split(delim)
         clean_address = ' '.join(split_raw)
-        data.append({"Address":clean_address, "Price":price.text, "Area":area.text})
+        data.append({"Address":clean_address, "Price":price.text, "Area":area.text + " m²"})
     for d in data:
         print(d)
 
+#Scraping through pages
 while(True):
     next_page_button = ffdriver.find_element(By.LINK_TEXT, '»')
     get_data()
-    time.sleep(1)
     wait.until(EC.element_to_be_clickable((next_page_button)))
     href_data = next_page_button.get_attribute('href')
     if href_data is None:
@@ -53,7 +55,4 @@ while(True):
     else:
         next_page_button.click()
 
-time.sleep(1)
-
-# print(ffdriver.)
 ffdriver.quit()
