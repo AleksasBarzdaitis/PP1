2321e66b24b19075e6d6469002b328ff378573bd
Aleksas <a.barzdaitis@gmail.com>
2022-06-13 15:54:28 +0300

    Added ability to select url to scrape from config file

--- src/main.py
+++ src/main.py
@@ -8,19 +8,26 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
+#Loading config file for logging
 with open('.\config\config.yml', 'r') as config:
     logging.config.dictConfig(yaml.safe_load(config)['logging'])
 
 main_logger = logging.getLogger('main')
 error_logger = logging.getLogger('error')
 
-#Loading a scraper
-url = 'https://www.aruodas.lt/namai/vilniuje/antakalnyje/'
+#Loading config file to get url to scrape
+with open('.\config\config.yml', 'r') as config:
+    url = yaml.safe_load(config)['urls']
+
+url_to_scrape = url['url']
+
+#Configuring scraper
+url = url_to_scrape
 op = webdriver.FirefoxOptions()
 ffdriver = webdriver.Firefox(service=FFService('src/drivers/geckodriver_v0.31.0.exe'), options=op)
 
+#Opening url to scrape and bypassing accept cookies pop-up
 target_button = "button[id*='onetrust-accept-btn-handler']"
-
 ffdriver.get(url)
 wait = WebDriverWait(ffdriver, 15)
 wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, target_button)))
@@ -32,10 +39,12 @@ def get_data():
     rows = ffdriver.find_elements(By.CSS_SELECTOR, '*.list-row')
     data = []
 
+    #Waiting for page to load
     wait.until(EC.text_to_be_present_in_element(
-        (By.CSS_SELECTOR, '#actionBarAdverts > div > h1'),
-        'Namai Vilniuje, Antakalnyje'))
+        (By.CSS_SELECTOR, 'tr.search-mark > td > div > div.text'),
+        'SKELBIMŲ'))
 
+    #Scraping info
     for row in rows:
         try:
             address = row.find_element(By.CSS_SELECTOR, 'td.list-adress > h3 > a')
@@ -44,20 +53,34 @@ def get_data():
         except NoSuchElementException:
             error_logger.exception('No such element to select')
             continue
+        #Cleaning address text
         delim = "\\n"
         raw_text = "%r"%address.text
         split_raw = raw_text.split(delim)
-        clean_address = ' '.join(split_raw)
+        clean_address = str(' '.join(split_raw))
+        #Defining scraped info structure and adding to list
         data.append({"Address":clean_address, "Price":price.text, "Area":area.text + " m²"})
+
+    #Printing scraped info
     for d in data:
         print(d)
 
 #Scraping through pages
 while(True):
-    next_page_button = ffdriver.find_element(By.LINK_TEXT, '»')
     get_data()
-    current_page = ffdriver.find_element(By.CSS_SELECTOR, 'a.active-page')
-    main_logger.info(f'Page {current_page.text} scraped successfully')
+    try:
+        current_page = ffdriver.find_element(By.CSS_SELECTOR, 'a.active-page')
+    except NoSuchElementException:
+        error_logger.exception('No such element to select')
+    try:
+        main_logger.info(f'Page {current_page.text} scraped successfully')
+    except NameError:
+        main_logger.info(f'Page 1 scraped successfully')
+    try:
+        next_page_button = ffdriver.find_element(By.LINK_TEXT, '»')
+    except NoSuchElementException:
+        main_logger.info(f'All available pages has been scraped successfully')
+        break
     wait.until(EC.element_to_be_clickable((next_page_button)))
     href_data = next_page_button.get_attribute('href')
     if href_data is None:
